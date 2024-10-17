80295e51c3ec7dba54fb118b88dff25d5baf7ef6
diguage <leejun119@gmail.com>
2024-10-17 23:06:20 +0800

    配置 EFK 日志套件

--- clean.sh
+++ clean.sh
@@ -9,3 +9,12 @@ docker container rm -f nacos
 docker container rm -f prometheus
 
 docker container rm -f grafana
+
+
+rm -rf ./data/elasticsearch/*
+
+docker container rm -f elasticsearch
+
+docker container rm -f kibana
+
+docker container rm -f fluentd
